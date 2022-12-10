df25887343ff276aa3c8cb9bb8769dd0967f5497
FulPlan <mutasimplan@protonmail.com>
2022-12-10 03:42:56 -0500

    Try

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-
+<<<<<<< HEAD
 """the `0-hello_route` module
 starts a flask web application listening on `0.0.0.0:5000`
 """
@@ -18,7 +18,7 @@ def hello():
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
-
+=======
 """
 starts a Flask web application
 """
@@ -34,4 +34,4 @@ def index():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
-    
+>>>>>>> 212529db4b955a72d996660137790c2497399ff5
