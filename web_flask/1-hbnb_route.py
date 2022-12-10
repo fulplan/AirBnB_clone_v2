7b8d596234d8beb3aa5a8f299cd38972760ce017
FulPlan <mutasimplan@protonmail.com>
2022-12-10 03:44:57 -0500

    Test

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,30 +1,30 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
-"""the `1-hbnb_route` module
-starts a flask web application listening on `0.0.0.0:5000`
-"""
+# <<<<<<< HEAD
+# """the `1-hbnb_route` module
+# starts a flask web application listening on `0.0.0.0:5000`
+# """
 
-from flask import Flask
+# from flask import Flask
 
-app = Flask(__name__)
-app.url_map.strict_slashes = False
-
-
-@app.route("/", strict_slashes=False)
-def index():
-    """returns `Hello HBNB!` message"""
-    return "Hello HBNB!"
+# app = Flask(__name__)
+# app.url_map.strict_slashes = False
 
 
-@app.route("/hbnb", strict_slashes=False)
-def hbnb():
-    """returns `HBNB` message"""
-    return "HBNB"
+# @app.route("/", strict_slashes=False)
+# def index():
+#     """returns `Hello HBNB!` message"""
+#     return "Hello HBNB!"
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
-=======
+# @app.route("/hbnb", strict_slashes=False)
+# def hbnb():
+#     """returns `HBNB` message"""
+#     return "HBNB"
+
+
+# if __name__ == "__main__":
+#     app.run(host="0.0.0.0", port=5000)
+# =======
 """
 starts a Flask web application
 """
@@ -46,4 +46,4 @@ def hbnb():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
->>>>>>> 212529db4b955a72d996660137790c2497399ff5
+# >>>>>>> 212529db4b955a72d996660137790c2497399ff5
